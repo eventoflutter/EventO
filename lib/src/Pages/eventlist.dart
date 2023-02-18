08ac2f929bb4ae75ab928bdf193f46b16446cd22
Vivekflutter <er.vivekpatil7@gmail.com>
2023-02-18 15:39:08 +0530

    Vivek's 2nd commit

---
+++ lib/src/Pages/eventlist.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+class eventlist extends StatefulWidget {
+  const eventlist({super.key});
+
+  @override
+  State<eventlist> createState() => _eventlistState();
+}
+
+class _eventlistState extends State<eventlist> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
